865cfbd20dd7e74ccde25a9a2442f140dd105d91
ivans <ivan.samoylov@gmail.com>
2025-08-31 17:37:20 +0500

    Add annotations. Add method to process questions and answers

--- hw02-annotation-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,10 +1,16 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
 
+import java.util.List;
+
+@Service
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
@@ -18,11 +24,28 @@ public class TestServiceImpl implements TestService {
         ioService.printFormattedLine("Please answer the questions below%n");
         var questions = questionDao.findAll();
         var testResult = new TestResult(student);
-
-        for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
+        for (var question : questions) {
+            var isAnswerValid = processQuestion(question);
             testResult.applyAnswer(question, isAnswerValid);
         }
         return testResult;
+    }
+
+    private boolean processQuestion(Question question) {
+        ioService.printFormattedLine("Question: %s%n", question.text());
+        List<Answer> answers = question.answers();
+        for (int i = 0; i < answers.size(); i++) {
+            ioService.printFormattedLine("\t%d: %s", i + 1, answers.get(i).text());
+        }
+        ioService.printLine("");
+        try {
+            int answerIndex = ioService.readIntForRangeWithPrompt(1, answers.size(), "Select correct number",
+                    "Incorrect number. Please try again");
+            var studentAnswer = answers.get(answerIndex);
+            return studentAnswer.isCorrect();
+        } catch (IllegalArgumentException e) {
+            ioService.printLine("Exceeding number of attempts for the question.");
+        }
+        return false;
     }
 }
